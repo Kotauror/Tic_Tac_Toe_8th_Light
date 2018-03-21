2d7444f50c3820f178106a7674677fa2a4ea4372
kotpsot <zygmunt.justyna@gmail.com>
2018-03-21 17:57:42 +0000

    Added player1 and player2 attributes to the Game class.

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,11 @@
 class Game
 
-  attr_reader :mode
+  attr_reader :mode, :player1, :player2
 
   def initialize
     @mode = nil
+    @player1 = nil
+    @player2 = nil
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,8 +4,16 @@ describe Game do
 
   subject(:game) { described_class.new }
 
-  it 'has initial mode of nil' do
-    expect(game.mode).to eq(nil)
+  describe '#initialize' do
+    it 'has initial mode nil' do
+      expect(game.mode).to eq(nil)
+    end
+    it 'has empty player1 slot' do
+      expect(game.player1).to eq(nil)
+    end
+    it 'has empty player2 slot' do
+      expect(game.player2).to eq(nil)
+    end
   end
 
 end
