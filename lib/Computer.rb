cfb37454f6534ff1d3043e279863fd6192398560
kotpsot <zygmunt.justyna@gmail.com>
2018-03-25 19:42:17 +0100

    Add random_move and elaborate_move methods to Computer class.

--- lib/Computer.rb
+++ lib/Computer.rb
@@ -3,8 +3,19 @@ require_relative 'player'
 class Computer < Player
 
   def select_number(board)
-    sleep(1)
-    board.available_numbers.sample
+    num = [1,0].sample
+    if num == 1
+      return random_move(board)
+    else
+      return elaborate_move(board)
+    end
+  end
+
+  def random_move(board)
+    # board.available_numbers.sample
+  end
+
+  def elaborate_move(board)
   end
 
 end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -2,5 +2,22 @@ require 'computer'
 
 describe Computer do
 
+  subject(:computer) { described_class.new("Computer 1", "C") }
+  let(:board) {double('board', :values => ["1", "2", "3", "4", "5", "6", "7", "8", "9"])}
+
+  context 'During the game' do
+    describe 'select_number' do
+      it 'selects the elaborate mode' do
+        expect(computer).to receive(:elaborate_move).with board
+        srand(5)
+        computer.select_number(board)
+      end
+      it 'selects the random mode' do
+        expect(computer).to receive(:random_move).with board
+        srand(50)
+        computer.select_number(board)
+      end
+    end
+  end
 
 end
