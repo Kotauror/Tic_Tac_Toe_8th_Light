f603fefc9df4dc87ba4420940ba5d26dac69c976
kotpsot <zygmunt.justyna@gmail.com>
2018-03-26 12:21:39 +0100

    Added tests for pick_winning_posiiton and block_opponent methods in Computer class.

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -21,35 +21,45 @@ describe Computer do
         computer.select_number(board, "K")
       end
     end
-  end
-  describe 'pick_5_when_possible' do
-    it 'picks 5 on board when 5 is available' do
-      allow(board).to receive(:available_numbers).and_return(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
-      expect(computer.pick_5_when_possible(board)).to eq("5")
+    describe 'pick_5_when_possible' do
+      it 'picks 5 on board when 5 is available' do
+        allow(board).to receive(:available_numbers).and_return(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
+        expect(computer.pick_5_when_possible(board)).to eq("5")
+      end
+      it 'doesn\'t pick 5 when 5 is not available' do
+        allow(board_without_5).to receive(:available_numbers).and_return(["1", "2", "3", "4", "J", "6", "7", "8", "9"])
+        expect(computer.pick_5_when_possible(board_without_5)).not_to eq("5")
+      end
     end
-    it 'doesn\'t pick 5 when 5 is not available' do
-      allow(board_without_5).to receive(:available_numbers).and_return(["1", "2", "3", "4", "J", "6", "7", "8", "9"])
-      expect(computer.pick_5_when_possible(board_without_5)).not_to eq("5")
+    describe 'pick_winning_position' do
+      it 'picks a winning position when possible' do
+        allow(board_winning_for_active).to receive(:available_numbers).and_return(["3", "4", "5", "6", "7", "8", "9"])
+        allow(board_winning_for_active).to receive(:put_sign_on_board).with("C", "3")
+        allow(board_winning_for_active).to receive(:is_game_won?).and_return true
+        allow(board_winning_for_active).to receive(:put_sign_on_board).with("3", "3")
+        expect(computer.pick_winning_position(board_winning_for_active)).to eq("3")
+      end
+      it 'doesn\'t pick a winning position if not possible' do
+        allow(board).to receive(:available_numbers).and_return(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
+        allow(board).to receive(:put_sign_on_board).with("C", "3")
+        allow(board).to receive(:is_game_won?).and_return false
+        allow(board).to receive(:put_sign_on_board).with("3", "3")
+      end
+    end
+    describe 'block_opponent' do
+      it 'blocks opponent when possible' do
+        allow(board_winning_for_passive).to receive(:available_numbers).and_return(["3", "4", "5", "6", "7", "8", "9"])
+        allow(board_winning_for_passive).to receive(:put_sign_on_board).with("K", "3")
+        allow(board_winning_for_passive).to receive(:is_game_won?).and_return true
+        allow(board_winning_for_passive).to receive(:put_sign_on_board).with("3", "3")
+        expect(computer.block_opponent(board_winning_for_passive, "K")).to eq("3")
+      end
+      it 'doesn\'t block an if not possible' do
+        allow(board).to receive(:available_numbers).and_return(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
+        allow(board).to receive(:put_sign_on_board).with("K", "3")
+        allow(board).to receive(:is_game_won?).and_return false
+        allow(board).to receive(:put_sign_on_board).with("3", "3")
+      end
     end
   end
-  describe 'pick_winning_position' do
-    it 'picks a winning position when possible' do
-      allow(board_winning_for_active).to receive(:available_numbers).and_return(["3", "4", "5", "6", "7", "8", "9"])
-      allow(board_winning_for_active).to receive(:put_sign_on_board).with("C", "3")
-      allow(board_winning_for_active).to receive(:is_game_won?).and_return true
-      allow(board_winning_for_active).to receive(:put_sign_on_board).with("3", "3")
-      expect(computer.pick_winning_position(board_winning_for_active)).to eq("3")
-    end
-  end
-  describe 'block_opponent' do
-    it 'blocks opponent when possible' do
-      allow(board_winning_for_passive).to receive(:available_numbers).and_return(["3", "4", "5", "6", "7", "8", "9"])
-      allow(board_winning_for_passive).to receive(:put_sign_on_board).with("K", "3")
-      allow(board_winning_for_passive).to receive(:is_game_won?).and_return true
-      allow(board_winning_for_passive).to receive(:put_sign_on_board).with("3", "3")
-      expect(computer.block_opponent(board_winning_for_passive, "K")).to eq("3")
-    end
-  end
-
-
 end
